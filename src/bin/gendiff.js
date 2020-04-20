4a48722185865f98faeb45d16710407ce6e7f522
dealmaker <alex.likhvar@gmail.com>
2020-04-20 11:54:12 +0300

    add args, actions, options to bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
-import program from 'commander';
 import { version, description } from '../../package.json';
+
+const { program } = require('commander');
 
 program
   .version(version)
   .description(description)
+  .arguments('<firstConfig> <secondConfig>')
+  .action(function (firstConfig, secondConfig) {
+    console.log(`${firstConfig}; ${secondConfig}`);
+  })
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
-  
