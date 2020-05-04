0e009bc0f14ff94e1e7a791b6c1750ddcc52920d
dealmaker <alex.likhvar@gmail.com>
2020-05-04 22:11:15 +0300

    add fixtures dir

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import genDiff from '../src/index';
 
-const before = '/home/alexander/Projects/gendiff/__tests__/before.json';
-const after = '/home/alexander/Projects/gendiff/__tests__/after.json';
-const result = fs.readFileSync('__tests__/result.txt', 'utf-8');
+const before = '/home/alexander/Projects/gendiff/__fixtures__/before.json';
+const after = '/home/alexander/Projects/gendiff/__fixtures__/after.json';
+const result = fs.readFileSync('__fixtures__/result.txt', 'utf-8');
 
 test('generate difference', () => {
   expect(genDiff(before, after)).toEqual(result);
