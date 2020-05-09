d8081142b052d547dd5b7b692162c283de1f920d
dealmaker <alex.likhvar@gmail.com>
2020-05-09 18:01:28 +0300

    adapt for ".ini" extension

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,18 @@ const beforeJson = '__fixtures__/before.json';
 const afterJson = '__fixtures__/after.json';
 const beforeYaml = '__fixtures__/before.yml';
 const afterYaml = '__fixtures__/after.yml';
+const beforeIni = '__fixtures__/before.ini';
+const afterIni = '__fixtures__/after.ini';
 const result = fs.readFileSync('__fixtures__/result.txt', 'utf-8');
 
-test('generate difference', () => {
+test('generate difference (flatten JSON)', () => {
   expect(genDiff(beforeJson, afterJson)).toEqual(result);
+});
+test('generate difference (flatten YAML)', () => {
   expect(genDiff(beforeYaml, afterYaml)).toEqual(result);
   expect(genDiff(beforeJson, afterYaml)).toEqual(result);
+});
+test('generate difference (flatten INI)', () => {
+  expect(genDiff(beforeIni, afterIni)).toEqual(result);
+  expect(genDiff(beforeYaml, afterIni)).toEqual(result);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,9 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const getJsonData = (absPath) => JSON.parse(fs.readFileSync(absPath, 'utf-8'));
 const getYamlData = (absPath) => yaml.safeLoad(fs.readFileSync(absPath, 'utf8'));
+const getIniData = (absPath) => ini.parse(fs.readFileSync(absPath, 'utf-8'));
 
 export default (pathToFile) => {
   const absPath = path.isAbsolute(pathToFile)
@@ -13,6 +15,7 @@ export default (pathToFile) => {
   if (path.extname(absPath) === '.yml') {
     return getYamlData(absPath);
   }
-
-  return getJsonData(absPath);
+  if (path.extname(absPath) === '.ini') {
+    return getIniData(absPath);
+  } return getJsonData(absPath);
 };
