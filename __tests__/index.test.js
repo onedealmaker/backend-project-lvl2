398a933cb5c2743e2bf0dca2a198d4a65346d34e
Alex Li <alex.likhvar@gmail.com>
2020-05-17 21:03:34 +0300

    add .each to tests & add new fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,24 @@
 import fs from 'fs';
 import genDiff from '../src/index';
 
-const beforeJson = '__fixtures__/flatten/before.json';
-const afterJson = '__fixtures__/flatten/after.json';
-const beforeYaml = '__fixtures__/flatten/before.yml';
-const afterYaml = '__fixtures__/flatten/after.yml';
-const beforeIni = '__fixtures__/flatten/before.ini';
-const afterIni = '__fixtures__/flatten/after.ini';
-const result = fs.readFileSync('__fixtures__/flatten/result.txt', 'utf-8');
+const flatData = [
+  ['__fixtures__/flatten/before.json', '__fixtures__/flatten/after.json'],
+  ['__fixtures__/flatten/before.yml', '__fixtures__/flatten/after.yml'],
+  ['__fixtures__/flatten/before.ini', '__fixtures__/flatten/after.ini'],
+];
+const nestedData = [
+  ['__fixtures__/nested/before.json', '__fixtures__/nested/after.json'],
+  ['__fixtures__/nested/before.yml', '__fixtures__/nested/after.yml'],
+  ['__fixtures__/nested/before.ini', '__fixtures__/nested/after.ini'],
+]
+const result = {
+  flat: fs.readFileSync('__fixtures__/flatten/result.txt', 'utf-8'),
+  tree: fs.readFileSync('__fixtures__/nested/result.txt', 'utf-8'),
+};
 
-test('generate difference (flat JSON)', () => {
-  expect(genDiff(beforeJson, afterJson)).toEqual(result);
+test.each(flatData)('generate difference (flat)', (a, b) => {
+  expect(genDiff(a, b)).toBe(result.flat);
 });
-test('generate difference (flat YAML)', () => {
-  expect(genDiff(beforeYaml, afterYaml)).toEqual(result);
-});
-test('generate difference (flat INI)', () => {
-  expect(genDiff(beforeIni, afterIni)).toEqual(result);
+test.each(nestedData)('generate difference (nested)', (a, b) => {
+  expect(genDiff(a, b)).toBe(result.flat);
 });
