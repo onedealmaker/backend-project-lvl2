e333ce85349e7015e286d0350e5dca42f2150d7f
Alex Li <alex.likhvar@gmail.com>
2020-05-31 18:30:02 +0300

    make file for launch only

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,15 +1,4 @@
 #!/usr/bin/env node
-import { version, description } from '../../package.json';
-import gendiff from '..';
+import launch from '..';
 
-const { program } = require('commander');
-
-program
-  .version(version)
-  .description(description)
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(gendiff(firstConfig, secondConfig));
-  })
-  .option('-f, --format [type]', 'output format')
-  .parse(process.argv);
+launch();
