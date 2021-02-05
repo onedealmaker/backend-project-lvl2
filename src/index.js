30ad61c2a7d1e94bce5f33f868b390845fb8ee92
onedealmaker <alex.likhvar@gmail.com>
2021-02-05 17:36:55 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -7,5 +7,7 @@ export default (pathToFileBefore, pathToFileAfter, format) => {
   const before = getData(pathToFileBefore);
   const after = getData(pathToFileAfter);
   const difference = buildDiff(before, after);
+  console.log(formatter);
+  console.log(formatter[format]);
   return formatter[format](difference);
 };
