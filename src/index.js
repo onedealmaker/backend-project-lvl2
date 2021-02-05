24666ef0ebc0016a6c4602e242c10a9e782df4f9
onedealmaker <alex.likhvar@gmail.com>
2021-02-05 17:52:46 +0300

    identyfy the default format directly in function genDiff

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import buildDiff from './diffBuilder';
 import formatter from './formatters';
 
 const getData = (pathToFile) => parse(pathToFile);
-export default (pathToFileBefore, pathToFileAfter, format) => {
+export default (pathToFileBefore, pathToFileAfter, format = 'stylish') => {
   const before = getData(pathToFileBefore);
   const after = getData(pathToFileAfter);
   const difference = buildDiff(before, after);
-  console.log(format);
+  console.log('just format', format);
   console.log(formatter[format]);
   return formatter[format](difference);
 };
