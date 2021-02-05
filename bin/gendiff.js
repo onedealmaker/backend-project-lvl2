29243d6dfa5ab8570ee0b442db21ebc71fd1d0cf
onedealmaker <alex.likhvar@gmail.com>
2021-02-05 15:23:52 +0300

    fix import in tests

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
-import launch from '../src/index.js';
+import commander from 'commander';
+import genDiff from '../src/index';
 
-launch();
+const { program } = commander;
+
+program
+  .version('1.1.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(genDiff(firstConfig, secondConfig, program.format));
+  })
+  .option('-f, --format [type]', 'output format', 'stylish');
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-import program from 'commander';
 import parse from './parsers';
 import buildDiff from './diffBuilder';
 import formatter from './formatters/index';
 
 const getData = (pathToFile) => parse(pathToFile);
-export const genDiff = (pathToFileBefore, pathToFileAfter, format) => {
+export default (pathToFileBefore, pathToFileAfter, format) => {
   const before = getData(pathToFileBefore);
   const after = getData(pathToFileAfter);
   const difference = buildDiff(before, after);
   return formatter[format](difference);
-};
-
-export default () => {
-  program
-    .version('1.1.1', '-V, --version', 'output the version number')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments('<firstConfig> <secondConfig>')
-    .action((firstConfig, secondConfig) => {
-      console.log(genDiff(firstConfig, secondConfig, program.format));
-    })
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .parse(process.argv);
 };
