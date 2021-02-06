fa5c9e6e845aae023fe5d7864987d863ae2f412a
onedealmaker <alex.likhvar@gmail.com>
2021-02-06 15:58:19 +0300

    test

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ export default (pathToFileBefore, pathToFileAfter, format = 'stylish') => {
   const after = getData(pathToFileAfter);
   const difference = buildDiff(before, after);
   console.log(before, after);
+  console.log('before doge object', before.common.setting6.doge);
+  console.log('after objects', after.common.setting6.doge, after.group3.deep.id);
   return formatter[format](difference);
 };
