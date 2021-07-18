751d42dc61000f3558d48daba0a8c8fa022ed5d4
onedealmaker <alex.likhvar@gmail.com>
2021-07-18 21:43:34 +0300

    fix actions check

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
+import * as fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
