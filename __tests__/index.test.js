c050e6e429a2cf56e659be7e7205273463e65e5f
onedealmaker <alex.likhvar@gmail.com>
2021-07-18 23:41:34 +0300

    fix bin file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,23 +8,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const filesToCompare = [
-  { file1: 'file1.json', file2: 'file2.json' },
-  { file1: 'file1.yml', file2: 'file2.yml' },
-  { file1: 'file1.json', file2: 'file2.yml' },
+  { before: 'before.json', after: 'after.json' },
+  { before: 'before.yml', after: 'after.yaml' },
+  { before: 'before.json', after: 'after.yaml' },
 // { before: 'before.ini', after: 'after.ini' },
 ];
 
-const resultStylish = fs.readFileSync(getFixturePath('result_stylish.txt'), 'utf-8');
-const resultPlain = fs.readFileSync(getFixturePath('result_plain.txt'), 'utf-8');
-// const resultJson = fs.readFileSync(getFixturePath('resultJSON.txt'), 'utf-8');
+const resultStylish = fs.readFileSync(getFixturePath('resultStylish.txt'), 'utf-8');
+const resultPlain = fs.readFileSync(getFixturePath('resultPlain.txt'), 'utf-8');
+const resultJson = fs.readFileSync(getFixturePath('resultJSON.txt'), 'utf-8');
 
-test.each(filesToCompare)('genDiff($file1, $file2)', ({ file1, file2 }) => {
-  expect(genDiff(getFixturePath(file1), getFixturePath(file2)))
+test.each(filesToCompare)('genDiff($before, $after)', ({ before, after }) => {
+  expect(genDiff(getFixturePath(before), getFixturePath(after)))
     .toEqual(resultStylish);
-  expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'stylish'))
+  expect(genDiff(getFixturePath(before), getFixturePath(after), 'stylish'))
     .toEqual(resultStylish);
-  expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'plain'))
+  expect(genDiff(getFixturePath(before), getFixturePath(after), 'plain'))
     .toEqual(resultPlain);
-  // expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'json'))
-  //   .toEqual(resultJson);
+  expect(genDiff(getFixturePath(before), getFixturePath(after), 'json'))
+    .toEqual(resultJson);
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,16 @@
 import commander from 'commander';
 import genDiff from '../src';
 
-const { program } = commander;
-
+const program = new commander.Command();
 program
-  .version('1.1.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(genDiff(firstConfig, secondConfig, program.format));
-  })
-  .option('-f, --format [type]', 'output format', 'stylish');
+  .version('0.0.1')
+  .addOption(new commander.Option('-f, --format [type]', 'output format')
+    .default('stylish', 'stylish')
+    .choices(['stylish', 'plain', 'json']))
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(
+    genDiff(filepath1, filepath2, program.opts().format),
+  ));
+
 program.parse(process.argv);
